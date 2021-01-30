96382b665f8078a903c79839f01eb57d2ae08bd9
David Hernando Sancha <david.hernando@2manylanguages.com>
2021-01-30 20:35:00 +0100

    [DUX_MAME] Fix building mame with the emscripten version 2.0.12

--- src/osd/sdl/sdlmain.cpp
+++ src/osd/sdl/sdlmain.cpp
@@ -8,6 +8,21 @@
 //
 //============================================================
 
+#if defined(__EMSCRIPTEN__)
+
+#include <emscripten/emscripten.h>
+#include <emscripten/bind.h>
+
+std::string getExceptionMessage(intptr_t exceptionPtr) {
+  return std::string(reinterpret_cast<std::exception *>(exceptionPtr)->what());
+}
+
+EMSCRIPTEN_BINDINGS(Bindings) {
+  emscripten::function("getExceptionMessage", &getExceptionMessage);
+};
+
+#endif
+
 // only for oslog callback
 #include <functional>
 
